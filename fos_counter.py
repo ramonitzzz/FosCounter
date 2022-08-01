4f7a39992a76637f669cf253a417e64d0eed26eb
Romina <79769611+ramonitzzz@users.noreply.github.com>
2022-08-01 13:54:39 -0700

    Update fos_counter.py

--- fos_counter.py
+++ fos_counter.py
@@ -1,5 +1,4 @@
 #%% <import modules>
-import nd2
 import skimage as sk
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,7 +7,9 @@ import os
 import pandas as pd
 import math
 from scipy import ndimage as ndi
+from scipy import stats
 import imageio
+import nd2
 
 #%% <functions>
 def fos_img(data_path):
@@ -16,27 +17,38 @@ def fos_img(data_path):
         vol= nd2.imread(data_path)
     else:
         vol=imageio.volread(data_path)
-    fos=vol[:,1,:,:]
+    fos=vol[:,3,:,:] #change the 3 here for the channel where your fos is stained, note that the count starts in 0 so if you have fos in the first channel of your image you need to put 0 here and so on (i.e here the 3 corresponds to the IR channel)
     stacks= np.shape(fos)[0]
     return fos, stacks
 
 
-def filtering(img):
+def filtering(img, top_thresh, low_thresh):
     denoise=sk.restoration.denoise_wavelet(img)
     blurred = sk.filters.gaussian(denoise, sigma=2.0)
-    clahe= sk.exposure.equalize_adapthist(blurred, kernel_size=127,clip_limit=0.01,  nbins=256)
-    t_thresh=sk.filters.threshold_otsu(clahe)
-    if t_thresh/np.median(clahe)<=1.45:
-        thresh= t_thresh + np.percentile(clahe, top_thresh)
+    t_thresh=sk.filters.threshold_otsu(blurred)
+    if np.percentile(blurred, 99)<= 0.0035: #this is a safety net for images with too little intensity, you can try and adjust this if needed
+        thresh= t_thresh + np.percentile(blurred, 95) 
+    elif np.percentile(blurred,99)>=0.01:
+        thresh= t_thresh + np.percentile(blurred, 4) #this is a safety net for images with too much intensity, you can also try and adjust this if needed 
     else:
-        thresh= t_thresh + np.percentile(clahe, low_thresh)
-    fos_cells=np.where(clahe >=thresh, 1, 0)
+        if t_thresh/np.median(blurred)>=3:
+            thresh= t_thresh
+        else:
+            if t_thresh/np.median(blurred)<=1.5: 
+                if t_thresh/np.median(blurred)<=0.99:
+                    thresh=t_thresh + np.percentile(blurred, 90)
+                else:
+                    thresh= t_thresh + np.percentile(blurred, top_thresh)
+            else:
+                thresh= t_thresh + np.percentile(blurred,low_thresh)
+    fos_cells=np.where(blurred >= thresh, 1, 0)
     filtered=ndi.median_filter(fos_cells, size=5)
     eroded=ndi.binary_erosion(filtered)
     dilated= ndi.binary_dilation(eroded, iterations=1)
     eroded=ndi.binary_erosion(dilated, iterations=2)
     filt=ndi.median_filter(eroded, size=5)
     return filt
+
 
 def show_original_filt (original, filt):
     fig, (ax1, ax2) = plt.subplots(1,2)
@@ -45,17 +57,6 @@ def show_original_filt (original, filt):
     ax1.set_axis_off()
     ax2.set_axis_off()
     plt.tight_layout()
-
-def particle_analyzer (img):
-    contours= sk.measure.find_contours(img, .8)
-    label_image= sk.measure.label(img)
-    regions = sk.measure.regionprops(label_image)
-    #props = sk.measure.regionprops_table(label_image, properties=('eccentricity'))
-    props = sk.measure.regionprops_table(label_image)
-    props_table=pd.DataFrame(props)
-    counts=len(contours)
-    return props_table, counts
-
 
 def show_labels(img, img_original):
     label_image= sk.measure.label(img)
@@ -88,7 +89,7 @@ def blobs_coordinates(img, stacks):
     for i in range(stacks):
         blobs_coords=pd.DataFrame(columns=["x","y","z"])
         fos_c=img[i]
-        filt=filtering(fos_c)
+        filt=filtering(fos_c, top_thresh, low_thresh)
         labels = sk.measure.label(filt)
         props = sk.measure.regionprops_table(labels, properties=('centroid','axis_major_length','axis_minor_length', 'bbox', 'equivalent_diameter_area','label', 'eccentricity'))
         props_table=pd.DataFrame(props)
@@ -119,19 +120,20 @@ def overlap_coords(blobs, stacks, dist_thresh):
                             overlap = overlap.append(a_series, ignore_index=True)
     return overlap
 
-#%% <parameters>
+
+#%% you're gonna have to adjust the thresholding parameters depending on your images 
 axis_limit = 60
-dist_parameter=20
-axis_min=12
+dist_parameter=25
+axis_min=10.5
 circ=0.98
-axis_ratio= 0.5
-top_thresh=50 #top thresh 50 for test images; 95 for recent mem ds
-low_thresh=20 #low thresh 20 for test images; 91 for recent mem ds
+axis_ratio= 0.45
+top_thresh=95 #this is gonna be the threshold for images with higher background, adjust it based on your settings
+low_thresh=91 #this is gonna be the threshold for images with lower background, adjust it based on your settings
 
-#%% <remote memory images>
-path="/Users/romina/Library/CloudStorage/OneDrive-VrijeUniversiteitAmsterdam/internship_1/remote_memory/PV_IR_Fos_G_Nissl_B"
+#%% path to images
+path="/PathGoesHere/fos_images" #change this to the path to your images 
 
-# %%
+# %% this is gonna print all of the files in the path directory
 all_files=[]
 for filename in os.listdir(path):
     if filename.endswith(".tif"):
@@ -153,10 +155,36 @@ for filename in all_files:
         c_series = pd.Series(count_list, index = counts.columns)
         counts = counts.append(c_series, ignore_index=True)
     except:
-        print(filename + " not read")
-# %%
-print(counts.head())
-# %%
-counts.to_csv("/Users/romina/Desktop/counts_fos_trial_2.csv")
-# %%
+        print(filename + " not_read")
 
+# %%
+counts.to_csv("/PathGoesHere/counts_fos.csv") #add the path and name of the results file
+
+########
+#here is where you can see your images and try which threshold suits you best 
+ # try adjusting the top and low threshold values until you get optimal results
+# %% 
+data= path + "/"+"35_1.tif" #open an image here to examinate (note that you're gonna have to open a few to see how your images vary from each other)
+fos_t, stacks=fos_img(data)
+print(stacks)
+
+
+# %% here you can see the values of the image intensities 
+for i in range(stacks):
+    denoise=sk.restoration.denoise_wavelet(fos_t[i])
+    blurred = sk.filters.gaussian(denoise, sigma=2.0)
+    thresh=sk.filters.threshold_otsu(blurred)
+    print(thresh/np.median(blurred)) #note that this is what the conditional thresholding is based on, this is the relation to the otsu threshold method to the median value of the intensity of the image
+    print("25 p:", np.percentile(blurred, 25), "99 p:", np.percentile(blurred, 99)) #this will give you information about the range of the image intensity
+
+# %% TEST TRIAL 
+#here you can visually inspect the image and how it is being thresholded, and then adjust your thresholding values accordingly 
+for i in range(stacks):
+    fos_cc=fos_t[i]
+    filt=filtering(fos_cc, top_thresh, low_thresh)
+    show_labels(filt, fos_cc)
+
+# %% here you can see the fos count of the trial image 
+blobs= blobs_coordinates(fos_t, stacks)
+overlap=overlap_coords(blobs, stacks, dist_parameter)
+print(len(blobs)-len(overlap))
