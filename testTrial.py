b5a1626f8cd0709cef58caf20751d2352da8b27a
ramonitzzz <romina.a.d@hotmail.com>
2022-11-01 18:37:33 +0100

    Update testTrial.py
    added functionality for pv and mc

--- testTrial.py
+++ testTrial.py
@@ -15,7 +15,8 @@ import warnings
 warnings.filterwarnings('ignore')
 
 #%% path to folder with the images
-path="/Users/romina/Library/CloudStorage/OneDrive-VrijeUniversiteitAmsterdam/Y1/internship_1/remote_memory/NisslIR_mCherry_PVB_fosG"
+#path="/Users/juliavanadrichem/Desktop/Huidige telling/vtrap 3-5m_app_ps1"
+path="/Users/juliavanadrichem/Desktop/Huidige telling/vtrap 3-5m_app_ps1"
 #retrieve all files from dataset folder
 all_files=[]
 for filename in os.listdir(path):
@@ -31,77 +32,128 @@ print(all_files)
 
 #%% General parameters
 channelFos=1 #channel where Fos staining is
+channelMC=2 #channel where MC staining is
+channelPV=0 #channel where PV staining is
 axis_limit = 60
 dist_thresh=25
 axis_min=11
-axis_limit=60
-circ=0.98
+circ=0.95
 axis_ratio= 0.48
 
-#Fos parameters 
+axis_ratio_mc=0.3
+axis_min_mc=14
+
+axis_ratio_pv=0.4
+axis_min_pv=14
+
+
+#Fos parameters, if you want to filter more per image, then 
 fos_thresh={
     "is_intensity_low":0, #use 0 if you have images with a high background, change to 1 if your images have low intensity and you wish to enhance contrast 
-    "top_thresh":25, #this is gonna be the threshold for images with higher background, adjust it based on your settings
-    "mid_thresh":9,#this is gonna be the threshold for images with medium background, adjust it based on your settings
-    "low_thresh":30, #this is gonna be the threshold for images with lower background, adjust it based on your settings
-    "high_int_thresh":92,
+    "top_thresh": 98, #this is gonna be the percentile threshold for images with higher background, adjust it based on your settings
+    "mid_thresh": 98,#this is gonna be the percentile threshold for images with medium background, adjust it based on your settings
+    "low_thresh": 97, #this is gonna be the percentile threshold for images with lower background, adjust it based on your settings
+    "extra_bright_thresh": 98.5, #extra threshold, if you
+    "high_int_thresh": 90, #are your images super bright? Then increase this value
+    "low_int_thresh": 98.8 #are your images super holey? Then decrease this value
+}
+
+#MC parameters
+mc_thresh={
+    "is_intensity_low":0, # don't use for mcherry image analysis 
+    "extra_bright_thresh": 99,
+    "top_thresh":55, #this is gonna be the threshold for images with higher background, adjust it based on your settings
+    "mid_thresh":50,#this is gonna be the threshold for images with medium background, adjust it based on your settings
+    "low_thresh":20, #this is gonna be the threshold for images with lower background, adjust it based on your settings
+    "high_int_thresh":80,
     "low_int_thresh":30
 }
 
+#PV parameters
+pv_thresh={
+    "is_intensity_low":0, #leave at 0 for PV
+    "extra_bright_thresh": 50,
+    "top_thresh":55, #this is gonna be the threshold for images with higher background, adjust it based on your settings
+    "mid_thresh":50,#this is gonna be the threshold for images with medium background, adjust it based on your settings
+    "low_thresh":20, #this is gonna be the threshold for images with lower background, adjust it based on your settings
+    "high_int_thresh":80,
+    "low_int_thresh":30
+}
 #%% get intensity cut off values for fos filtering function
 fos_ints= intensitySaver(path, all_files, channelFos, fos_thresh.get("is_intensity_low")).getIntensityValues()
-print (fos_ints)
+print ("Fos intensity cutoffs: ",  fos_ints)
 
-#%%
-#select one image to visualize if the thresholding is correct
-test_image= "V_2_2006.nd2" #name of image file
+# get intensity cut off value for filtering function, for mc
+mc_ints= intensitySaver(path, all_files, channelMC, 0).getIntensityValues()
+print("MCherry intensity cutoffs: ", mc_ints)
+
+# get intensity cut off value for filtering function, for pv
+pv_ints=intensitySaver(path, all_files, channelMC, 0).getIntensityValues()
+print ("PV intensity cutoffs: ",pv_ints)
+os.system('say "your code has finished"');
+## visualize how your images look after being thresholded with the previous parameter
+
+#%% select one image to visualize if the thresholding is correct 
+test_image= "vtrap_remote memory 3-5m_B.tif" #name of image file (incl tif) 
 data= path + "/"+ test_image #open an image here to examinate (note that you're gonna have to open a few to see how your images vary from each other)
-fos_t, stacks=getImg(channelFos, data)
+fos_t, stacks=getImg(channelFos, data) #if you want to analyse the mcherry channel, put channelMC
+mc_t, stacks=getImg(channelMC, data)
+pv_t, stacks=getImg(channelPV, data)
 print(stacks) #will print the number of planes of the z-stack
 
-## visualize how your images look after being thresholded with the previous parameters
-#%% visually inspect the chosen image and how it is being thresholded, and then adjust your thresholding values accordingly 
+#%% Fos 
+#%% visually inspect the chosen image and how it is being thresholded for fos , and then adjust your thresholding values accordingly 
 for i in range(stacks):
     fos_cc=fos_t[i]
-    filt= getThresh(fos_cc).threshFos(fos_thresh, fos_ints)
+    filt= getThresh(fos_cc).thresh(fos_thresh, fos_ints)
     show_labels(filt, fos_cc, circ, axis_min, axis_limit, axis_ratio)
 
 #%%
-# print the counts for Fos cells
-blobs_fos= getCoords(fos_t, stacks, circ, axis_ratio, axis_min, axis_limit).coordsFos(fos_thresh, fos_ints)
+# print the counts for Fos cells 
+blobs_fos= getCoords(fos_t, stacks, circ, axis_ratio, axis_min, axis_limit).coordsCells(fos_thresh, fos_ints)
 overlap=getOverlap(stacks, dist_thresh).overlap_coords(blobs_fos)
 print(len(blobs_fos)-len(overlap))
 
 #%%
-#get values of each plane to assess where to adjust values
-intInfo= pd.DataFrame(columns=["stack","classifier value", "25 p", "99 p", "threshold applied"])
+#get values of each plane to assess where to adjust values 
+intInfoFos= getThresh(fos_t).intInfo(stacks, fos_ints)
+print(intInfoFos) #for Fos
+
+#%% MC 
+#select one image to visualize if the thresholding is correct 
+#%% visually inspect the chosen image and how it is being thresholded for mc, and then adjust your thresholding values accordingly 
 for i in range(stacks):
-    denoise=sk.restoration.denoise_wavelet(fos_t[i])
-    blurred = sk.filters.gaussian(denoise, sigma=2.0)
-    if fos_thresh.get("is_intensity_low") ==0:
-        prepro=blurred
-    else:
-        prepro= sk.exposure.equalize_adapthist(blurred, kernel_size=127,clip_limit=0.01,  nbins=256)
-    thresh=sk.filters.threshold_otsu(prepro)
-    if np.percentile(prepro, 25)<= fos_ints.get("low_int"): 
-        cat= "high_int_thresh"
-    elif np.percentile(prepro,99)>= fos_ints.get("high_int"):
-        cat= "low_int_thresh"
-    else:
-        if thresh/np.median(prepro)>=fos_ints.get("int_cutoff_up"):
-            cat= "no thresh"
-        else:
-            if thresh/np.median(prepro)<=fos_ints.get("int_cutoff"): 
-                if thresh/np.median(prepro)<=fos_ints.get("int_cutoff_down"): 
-                    cat= "low_thresh"
-                else:
-                    cat= "top thresh"
-            else:
-                cat= "mid thresh"
-    val_list= [i, thresh/np.median(prepro),np.percentile(prepro, 25), np.percentile(prepro, 99), cat]
-    c_series = pd.Series(val_list, index = intInfo.columns)
-    intInfo = intInfo.append(c_series, ignore_index=True)
-print(intInfo)
+    mc_cc=mc_t[i]
+    filt= getThresh(mc_cc).thresh(mc_thresh, mc_ints)
+    show_labels(filt, mc_cc, circ, axis_min_mc, axis_limit, axis_ratio_mc)
+
+#%%
+# print the counts for MC cells 
+blobs_mc=getCoords(mc_t, stacks, circ, axis_ratio_mc, axis_min_mc, axis_limit).coordsCells(mc_thresh, mc_ints)
+overlap=getOverlap(stacks, dist_thresh).overlap_coords(blobs_mc)
+print(len(blobs_mc)-len(overlap))
+
+#%%get values of each plane to assess where to adjust values
+#MCherry
+intInfoMC= getThresh(mc_t).intInfo(stacks, mc_ints)
+print(intInfoMC)
+
+#%% PV 
+# visually inspect the chosen image and how it is being thresholded for mc, and then adjust your thresholding values accordingly 
+for i in range(stacks):
+    pv_cc=pv_t[i]
+    filt= getThresh(pv_cc).thresh(pv_thresh, pv_ints)
+    show_labels(filt, pv_cc, circ, axis_min_mc, axis_limit, axis_ratio_mc)
+
+#%%
+# print the counts for MC cells 
+blobs_pv=getCoords(pv_t, stacks, circ, axis_ratio_mc, axis_min_mc, axis_limit).coordsCells(pv_thresh, pv_ints)
+overlap=getOverlap(stacks, dist_thresh).overlap_coords(blobs_pv)
+print(len(blobs_pv)-len(overlap))
+
+#%%get values of each plane to assess where to adjust values
+#PV
+intInfoPV= getThresh(pv_t).intInfo(stacks, pv_ints)
+print(intInfoPV)
 
 ## once you have decided on the thresholding values copy them on the main file and run the script to obtain the counts for the dataset
-
